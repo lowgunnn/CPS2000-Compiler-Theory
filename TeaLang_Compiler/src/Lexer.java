01a722446be541e15504e99e02cb05c1d530dc35
Logan Formosa <loganformosa11@gmail.com>
2021-06-23 02:43:15 +0200

    lexer lest sa tokenizer, must fix multi-line comment and string.num

--- TeaLang_Compiler/src/Lexer.java
+++ TeaLang_Compiler/src/Lexer.java
@@ -4,47 +4,28 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Lexer {
-	
-	
 
-	int delta = -10;
-	public int[][] transition_table = {
-			{ 1, 1, 3, 3, delta, delta, delta, delta, delta, delta, 10, delta, 12, 13, delta, 13, delta, 17, 17, 17,
-					delta, delta, delta },
-			{ 2, 3, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, 12, 13, delta, 13, delta, 17,
-					17, 17, delta, delta, delta },
-			{ 4, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, 12, 13, delta, 13, delta,
-					17, 17, 17, delta, delta, delta },
-			{ 5, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, 13, 12, 15, delta, 15, delta, 17,
-					17, 17, delta, delta, delta },
-			{ 6, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, 12, 13, delta, 13, delta,
-					17, 17, 17, delta, delta, delta },
-			{ 7, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, 12, 13, delta, 13, delta,
-					17, 17, 17, delta, delta, delta },
-			{ 8, delta, delta, delta, delta, delta, 9, 9, 9, delta, delta, delta, 12, 13, delta, 13, delta, 17, 17, 17,
-					delta, delta, delta },
-			{ 10, delta, delta, delta, delta, delta, delta, delta, delta, delta, 10, delta, 12, 13, delta, 13, delta,
-					17, 17, 17, delta, delta, delta },
-			{ 11, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, 12, 12, 13, delta, 16, delta,
-					17, 17, 17, delta, delta, delta },
-			{ delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, 12, 13, delta, 13,
-					delta, 18, 18, 18, delta, delta, delta },
-			{ 17, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, 12, 13, delta, 13, delta,
-					20, 19, 20, delta, delta, delta },
-			{ 21, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, 12, 13, delta, 13, delta,
-					17, 17, 17, delta, delta, delta },
-			{ delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, 14, 13, delta, 13,
-					delta, delta, delta, delta, delta, delta, delta },
-			{ 22, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta,
-					delta, delta, delta, delta, delta, delta, delta, delta },
-			{ 10, delta, delta, delta, delta, delta, delta, delta, delta, delta, 10, delta, 12, 13, delta, 13, delta,
-					17, 17, 17, delta, delta, delta },
-			{ delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, 12, 13, delta, 13,
-					delta, 17, 17, 17, delta, delta, delta },
-			{ delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta, delta,
-					delta, delta, delta, delta, delta, delta, delta, delta }
+	public static int[][] transition_table = {
+			{ 1, 1, 3, 3, -1, -1, -1, -1, -1, -1, 10, -1, 12, 13, -1, 13, -1, 17, 17, 17, -1, -1, -1 }, // digits
+			{ 2, 3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 12, 13, -1, 13, -1, 17, 17, 17, -1, -1, -1 }, // . as in 5.6
+			{ 4, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 12, 13, -1, 13, -1, 17, 17, 17, -1, -1, -1 }, //
+			{ 5, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 13, 12, 15, -1, 15, -1, 17, 17, 17, -1, -1, -1 },
+			{ 6, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 12, 13, -1, 13, -1, 17, 17, 17, -1, -1, -1 },
+			{ 7, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 12, 13, -1, 13, -1, 17, 17, 17, -1, -1, -1 },
+			{ 8, -1, -1, -1, -1, -1, 9, 9, 9, -1, -1, -1, 12, 13, -1, 13, -1, 17, 17, 17, -1, -1, -1 },
+			{ 10, -1, -1, -1, -1, -1, -1, -1, -1, -1, 10, -1, 12, 13, -1, 13, -1, 17, 17, 17, -1, -1, -1 },
+			{ 11, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 12, 12, 13, -1, 16, -1, 17, 17, 17, -1, -1, -1 },
+			{ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 12, 13, -1, 13, -1, 18, 18, 18, -1, -1, -1 },
+			{ 17, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 12, 13, -1, 13, -1, 20, 19, 20, -1, -1, -1 },
+			{ 21, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 12, 13, -1, 13, -1, 17, 17, 17, -1, -1, -1 },
+			{ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 14, 13, -1, 13, -1, -1, -1, -1, -1, -1, -1 },
+			{ 10, -1, -1, -1, -1, -1, -1, -1, -1, -1, 10, -1, 12, 13, -1, 13, -1, 17, 17, 17, -1, -1, -1 },
+			{ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 12, 13, -1, 13, -1, 17, 17, 17, -1, -1, -1 },
+
 
 	};
+
+	public static int[] final_states = { 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 16, 20, 21 };
 
 	public static int new_state_transition(int current_state, char encountered_char) {
 
@@ -53,68 +34,81 @@ public class Lexer {
 		// digits
 		if (ascii_value <= 57 && ascii_value >= 48) {
 
-			
+			return transition_table[0][current_state];
 
 		}
 		// letters
 		else if ((ascii_value <= 90 && ascii_value >= 65) || (ascii_value >= 97 && ascii_value <= 122)) {
-			
+			return transition_table[13][current_state];
 		}
 		// dot in numbers
-		else if(encountered_char == '.'){
-			
+		else if (encountered_char == '.') {
+			return transition_table[1][current_state];
 		}
-		//plus or minus
+		// plus or minus
 		else if ((encountered_char == '+' || encountered_char == '-')) {
-			
+			return transition_table[2][current_state];
 		}
-		//* for mulitplication or multi-line comments
+		// * for multiplication or multi-line comments
 		else if ((encountered_char == '*')) {
-			
+			return transition_table[3][current_state];
 
 		}
-		//Not operator
+		// Not operator
 		else if ((encountered_char == '!')) {
-			
+			return transition_table[4][current_state];
 		}
-		//comparison operators
+		// comparison operators
 		else if ((encountered_char == '>' || encountered_char == '<')) {
-			
+			return transition_table[5][current_state];
 		}
-		//equals sign 
+		// equals sign
 		else if ((encountered_char == '=')) {
-			
+			return transition_table[6][current_state];
 		}
-		//underscore for naming variables 
+		// underscore for naming variables
 		else if ((encountered_char == '_')) {
-			
-		}	
-		//division or comment slash
+			return transition_table[7][current_state];
+		}
+		// division or comment slash
 		else if (encountered_char == '/') {
-			
+			return transition_table[8][current_state];
 		}
 		// escpe cahracters slash
-		else if (encountered_char == '\\'){
-		
+		else if (encountered_char == '\\') {
+			return transition_table[9][current_state];
 		}
-		//iverted commas tal istrings
+		// iverted commas tal istrings
 		else if (encountered_char == '"') {
-			
+			return transition_table[10][current_state];
 		}
-		//one spacers
-		else if (encountered_char == ';' || encountered_char == ':' || encountered_char == '(' || encountered_char == ')' || encountered_char == '{' || encountered_char == '}'  ) {
-			
+		// one spacers
+		else if (encountered_char == ';' || encountered_char == ':' || encountered_char == '('
+				|| encountered_char == ')' || encountered_char == '{' || encountered_char == '}') {
+			return transition_table[11][current_state];
+		} 
+		else if (encountered_char == '\n') {
+			return transition_table[12][current_state];
 		}
-		else if(encountered_char == '\n') {
-			
+		// a printable character enclosed within " ", <- case for encountering
+		else if ((32 <= ascii_value) && (ascii_value <= 127)) {
+
+			return transition_table[14][current_state];
 		}
-		return 0;
+		// otherwise an unexpected character not in grammer, send 0 state
+		else {
+			return -1;
+		}
 
 	}
 
 	public static String readText(String filename) {
 		String data = "empty";
 		String entire = "";
+		String lexeme = "";
+		int state = 0;
+		int new_state;
+		boolean is_final = false;
 
 		try {
 
@@ -124,36 +118,52 @@ public class Lexer {
 			while (sc.hasNextLine()) {
 				line_number++;
 				data = sc.nextLine();
-				entire += data + "\n";
-				String lexeme = "";
-				int state = 0;
+				entire = data + "\n";
+				
+				for (int i = 0; i < entire.length(); i++) {
+					char encountered_char = entire.charAt(i);
 
-				for (int i = 0; i < data.length(); i++) {
-					char encountered_char = data.charAt(i);
+					// skips initials spaces or newlines before tokens, this does not include those
+					// inside " " string identifiers since previously we would have encountered an
+					// inverted comma
+					if (lexeme == ""
+							&& (encountered_char == ' ' || encountered_char == '\t' || encountered_char == '\n')) {
+						continue;
+					}
 
-					if (encountered_char == ' ') {
-						lexeme = "";
-						state = 0;
-						System.out.println("space");
-					} else {
-						if (encountered_char == '\t') {
+					new_state = new_state_transition(state, encountered_char);
+					
+					if (new_state == -1) {
+						System.out.println("New Token:");
+						// bad character, transitions to state delta
+
+						// check if current state is a final state, if it is then correct Token
+						is_final = false;
+						for (int final_state : final_states) {
+
+							if (final_state == state) {
+								is_final = true;
+							}
+
+						}
+
+						if (is_final) {
+
+							System.out.println(lexeme);
 							lexeme = "";
 							state = 0;
-							System.out.println("tabs");
+							i--;
 						} else {
-
-							if (encountered_char == '\n') {
-								lexeme = "";
-								state = 0;
-								System.out.println("NewLine");
-							} else {
-								System.out.println(encountered_char);
-								state = new_state_transition(state, encountered_char);
-								lexeme += encountered_char;
-
-							}
+							System.out.println("LEXICAL ERROR ENCOUNTERED AT LINE NUMBER: " + line_number);
+							break;
 						}
+
 					}
+					else {
+						lexeme += encountered_char;
+						state = new_state;
+					}
+
 				}
 
 			}
--- TeaLang_Compiler/src/Token.java
+++ TeaLang_Compiler/src/Token.java
@@ -6,4 +6,6 @@ public class Token {
 	int line_number;
 	
 	
+
+	
 }
