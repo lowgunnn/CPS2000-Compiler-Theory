240b972c5fad9477a9a29cbc93c399ffd929ea85
Logan Formosa <loganformosa11@gmail.com>
2021-06-23 04:32:26 +0200

    Actually Push the token changes ctrl-s duda

--- TeaLang_Compiler/src/Token.java
+++ TeaLang_Compiler/src/Token.java
@@ -23,7 +23,89 @@ public class Token {
 		case 3:
 			return "Float_Value";
 		case 4:
-			if(lexeme == +)
+			return "Addition";
+		case 5:
+			return "Asterisk";
+		case 7:
+			return "Comparison";
+		case 8:
+			return "Equals";
+		case 9:
+			return "Equality_Class";
+		case 10:
+			
+			//These are the "strings" so we must include reserved key words aswell
+			switch(lexeme) {
+				
+			case "let":
+				return "Variable_Assigner";
+			case "print":
+				return "Print_Keyword";
+			case "return":
+				return "Return_Keyword";
+			case "if":
+				return "If_Keyword";
+			case "for":
+				return "For_Keyword";
+			case "while":
+				return "While_Keyword";
+			case "float":
+				return "Float_Keyword";
+			case "int":
+				return "Int_Keyword";
+			case "bool":
+				return  "Bool_Keyword";
+			case "string":
+				return "String_Keyword";
+			case "true":
+				return "True_Keyword";
+			case "false":
+				return "False_Keyword";
+			case "and":
+				return "And_Keyword";
+			case "or":
+				return "Or_Keyword";
+			case "not":
+				return "Not_Keyword";
+				
+			
+			
+			}
+		case 11:
+			return "Division_Slash";
+		case 14:
+			return "Single_Line_Comment";
+		case 16:
+			return "Multi_Line_Comment";
+		case 20:
+			return "String_Value";
+		case 21:
+			
+			switch(lexeme) {
+			
+			case ";":
+				return "Semi_Colon";
+			case ":":
+				return "Colon";
+			case "(":
+				return "Opening_Bracket";
+			case ")":
+				return "Closing_Bracket";
+			case "{":
+				return "Opening_Curly";
+			case "}":
+				return "Closing_Curly";
+		
+			
+			}
+		case 22:
+			return "End_Of_File";
+		
+		default:
+			System.out.println("Unexpected Token Type Encountered!!");
+			return "Error_Token ??";
+					
+			
 			
 		}
 
