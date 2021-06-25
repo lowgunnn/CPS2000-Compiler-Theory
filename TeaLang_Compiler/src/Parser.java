fa0f1ef6746ff0cd2d135d4e06427fdba9defdd4
Logan Formosa <loganformosa11@gmail.com>
2021-06-25 02:31:33 +0200

    bdejna naqa parser backbone

--- TeaLang_Compiler/src/Parser.java
+++ TeaLang_Compiler/src/Parser.java
@@ -1,16 +1,15 @@
-import java.util.ArrayList;
-import java.util.Stack;
+import java.util.*;
 
 
 public class Parser {
 
 	
 	
-	String[] non_terminals = {"Program", "Block", "Statement", "VariableDecl", "Assignment" , "Print" ,"If" ,
+	static String[] non_terminals = {"Program", "Block", "Statement", "VariableDecl", "Assignment" , "Print" ,"If" ,
 			"For" , "While" , "Return" , "Function" , "Params" , "Param" , "Type" , "Literal", "Expression" ,
 			"SimpleExpression", "RelationalOp", "Term" , "AdditiveOp" , "Factor", "MultiplicativeOp" , "FunctionCall", "SubExpression",
 			"Unary","ActualParams"};
-	String[]terminals = { "Variable_Assigner", "Colon", "Equals", "Semi_Colon", "Print_Keyword" , "Return_Keyword" , "If_Keyword" , "Opening_Bracket" , "Closing_Bracket",
+	static String[]terminals = { "Variable_Assigner", "Colon", "Equals", "Semi_Colon", "Print_Keyword" , "Return_Keyword" , "If_Keyword" , "Opening_Bracket" , "Closing_Bracket",
 						"Opening_Curly", "Closing_Curly", "Else_Keyword", "For_Keyword" ,"While_Keyword", "Float_Keyword" , "Int_Keyword","Bool_Keyword",
 						"String_Keyowrd", "Variable_Identifier", "Integer_Value" , "Float_Value", "True_Keyword", "False_Keyword", "String_Value", "Not_Keyword", "Asterisk",
 						"Division_Slash", "And_Keyword", "Or_Keyword", "Comparison", "Equality_Class", "Addition"
@@ -20,7 +19,7 @@ public class Parser {
 	
 	//1-8 statements including block and return?? its in the EBNF
 	
-	int[][] parser_table ={ 
+	static int[][] parser_table ={ 
 		{1,0,0,0,2,6,3,0,0,8,0,0,4,5,7,7,7,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0},  //program
 		{0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},  //block
 		{1,0,0,0,2,6,3,0,0,8,0,0,4,5,7,7,7,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0},  //statement
@@ -51,6 +50,8 @@ public class Parser {
 	
 	public static void parseProgram(ArrayList<Token> lexedTokens) {
 		
+		
+		
 		System.out.println("~~~~~~~~~~~~~~PARSER~~~~~~~~~~~~~~~~~~~~~~~");
 		
 		Stack<String >stack = new Stack<String>();
@@ -59,14 +60,66 @@ public class Parser {
 		stack.push("Program");
 		
 		int no_of_tokens = lexedTokens.size();
+		int production_rule;
 		
+		int index1,index2;
 		
+		String last_terminal, last_non_terminal;
 		
+		Token current_token;
 		
 		for(int index = 0; index < no_of_tokens; index++) {
 			
+			current_token = lexedTokens.get(index);
 			
-			System.out.println(lexedTokens.get(index).value);
+			
+			
+			if(Arrays.asList(terminals).contains(stack.peek())) {
+				//top of stack is a terminal
+				
+				last_terminal = stack.pop();
+				
+				if(last_terminal != current_token.type) {
+					//Report a syntax error
+					
+					System.out.println("Syntax Error at line "+current_token.line_number);
+				}
+				
+				
+			}
+			else {
+				//it was a non-terminal
+				
+				last_non_terminal = stack.pop();
+				
+				index1 = Arrays.asList(non_terminals).indexOf(last_non_terminal);
+				index2 = Arrays.asList(terminals).indexOf(current_token.type);
+				
+				
+				production_rule = parser_table[index1][index2];
+				
+				if(production_rule == 0) {
+					System.out.println("Syntax error at line "+current_token.line_number);
+				}
+				else {
+					switch(production_rule) {
+					
+					
+					case 1:
+					case 2:
+						//etc ghalissa ha mmur norqod
+					
+					
+					}
+					
+					
+					
+				}
+				
+			}
+				
+			
+			
 			
 		}
 		
