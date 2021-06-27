59b631c6e61b810c506e75e0eab66233356eedc4
Logan Formosa <loganformosa11@gmail.com>
2021-06-27 07:35:40 +0200

    Almost done with Production Rules!

--- TeaLang_Compiler/src/Parser.java
+++ TeaLang_Compiler/src/Parser.java
@@ -7,7 +7,7 @@ public class Parser {
 	
 	static String[] non_terminals = {"Program", "Block", "Statement", "VariableDecl", "Assignment" , "Print" ,"If" ,
 			"For" , "While" , "Return" , "Function" , "Params" ,  "Type" , "Literal", "Expression" ,
-			"SimpleExpression", "RelationalOp", "Term" , "AdditiveOp" , "Factor", "MultiplicativeOp" , "FunctionCall", "SubExpression",
+			"SimpleExpression", "RelationalOp", "Term" , "AdditiveOp" , "Factor", "MultiplicativeOp" , "Return", "FunctionCall", "SubExpression",
 			"Unary","ActualParams","Else", "Comma"};
 	static String[]terminals = { "Variable_Assigner", "Colon", "Equals", "Semi_Colon", "Print_Keyword" , "Return_Keyword" , "If_Keyword" , "Opening_Bracket" , "Closing_Bracket",
 						"Opening_Curly", "Closing_Curly", "Else_Keyword", "For_Keyword" ,"While_Keyword", "Float_Keyword" , "Int_Keyword","Bool_Keyword",
@@ -35,23 +35,23 @@ public class Parser {
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,10,10,10,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, //type
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11,11,11,11,11,0,0,0,0,0,0,0,0,0,0}, //literal
 		{0,0,0,0,0,0,0,13,0,0,0,0,0,0,0,0,0,0,12,11,11,11,11,11,0,14,0,0,0,0,0,0,14,0,0}, //expression
-		{0,0,0,0,0,0,0,13,0,0,0,0,0,0,0,0,0,0,12,11,11,11,11,11,0,14,0,0,0,0,0,0,14,0,0}, //simple expression
-		{0,0,0,32,0,0,0,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,16,0,0,16,0,32},	//relationalOp
-		{0,0,0,0,0,0,0,13,0,0,0,0,0,0,0,0,0,0,12,11,11,11,11,11,0,14,0,0,0,0,0,0,14,0,0}, //Term
-		{0,0,0,31,0,0,0,0,31,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,16,0,0,16,0,31},			//additiveOp
-		{0,0,0,0,0,0,0,13,0,0,0,0,0,0,0,0,0,0,12,11,11,11,11,11,0,14,0,0,0,0,0,0,14,0,0}, //Factor
-		{0,0,0,30,0,0,0,0,30,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,17,17,17,0,0,0,0,0,30}, //MultipilicatievOp
+		{0,0,0,0,0,0,0,43,0,0,0,0,0,0,0,0,0,0,42,41,41,41,41,41,0,44,0,0,0,0,0,0,44,0,0}, //simple expression
+		{0,0,0,32,0,0,0,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,46,46,46,47,17,17,47,0,32},	//relationalOp
+		{0,0,0,0,0,0,0,39,0,0,0,0,0,0,0,0,0,0,38,37,37,37,37,37,0,40,0,0,0,0,0,0,40,0,0}, //Term
+		{0,0,0,31,0,0,0,0,31,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,45,45,45,17,32,32,17,0,31}, //additiveOp
+		{0,0,0,0,0,0,0,35,0,0,0,0,0,0,0,0,0,0,34,33,33,33,33,33,0,36,0,0,0,0,0,0,36,0,0}, //Factor
+		{0,0,0,30,0,0,0,0,30,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,17,17,17,32,31,31,32,0,30}, //MultipilicatievOp
 		{0,0,0,0,0,0,0,0,0,24,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, //Returns 
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,18,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, //Function Call
 		{0,0,0,0,0,0,0,19,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//SubExpression
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,20,0,0,0,0,0,0,20,0,0}, //Unary
-		{0,0,0,0,0,0,0,13,0,0,0,0,0,0,0,0,0,0,12,11,11,11,11,11,0,14,0,0,0,0,0,0,14,0,0},//ActualParams
+		{0,0,0,53,0,0,0,50,28,0,0,0,0,0,0,0,0,0,49,48,48,48,48,48,0,51,0,0,0,0,0,0,51,0,52},//ActualParams
 		{0,0,0,0,0,0,0,0,0,0,0,23,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, //Else-Statements
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0} //Comma
 		};
 	
 	
-	public static void parseSyntax(int production_rule) {
+	public static void parseSyntax(int production_rule, Token next_token) {
 		
 		//based on the production rule, the contents of the stack are adjusted
 		switch(production_rule) {
@@ -140,7 +140,7 @@ public class Parser {
 			
 		case 12:
 			
-			if(stack.peek()== "(") {
+			if(next_token.type== "Opening_Bracket") {
 				//function call
 				stack.push("SimpleExpression");
 				stack.push("RelationalOp");
@@ -148,9 +148,9 @@ public class Parser {
 				stack.push("AdditiveOp");
 				stack.push("Factor");
 				stack.push("MultiplicativeOp");
-				stack.push(")");
+				stack.push("Closing_Bracket");
 				stack.push("ActualParams");
-				stack.push("(");
+				stack.push("Opening_Bracket");
 			}else {
 				stack.push("SimpleExpression");
 				stack.push("RelationalOp");
@@ -184,8 +184,6 @@ public class Parser {
 			break;
 		case 17:
 			//Multiplicative Op Encountered
-			stack.push("MultiplicativeOp");
-			stack.push("Factor");
 			break;
 		//These cases may be numbered wrong because are either
 		// a) Production Rules brought about by use of Follow-Sets, I first started iplementing only those which make use of the FIRST Set
@@ -204,7 +202,7 @@ public class Parser {
 			break;
 		case 25:
 			// secondary variable declaration parse rule, this does not include adding other statement afterwards
-			//used for one-off for loop variable declarations
+			//used for one-off declarations like in for loop variable declarations
 			stack.push("Semi_Colon");
 			stack.push("Expression");
 			stack.push("Equals");
@@ -213,7 +211,7 @@ public class Parser {
 			stack.push("Variable_Identifier");
 			break;
 		case 26:
-			//variable assignment
+			//variable assignment   -> onestament ma nax kif insejta din lol
 			stack.push("Statement");
 			stack.push("Semi_Colon");
 			stack.push("Expression");
@@ -244,6 +242,7 @@ public class Parser {
 			stack.pop(); //TERM
 			stack.pop(); //RELATIONAL
 			stack.pop(); //SIMPLE EXP
+			if(stack.peek() != "ActualParams")
 			stack.pop(); //FOLLOW CHAR -> ; ) ,
 			break;
 		case 31:
@@ -251,14 +250,219 @@ public class Parser {
 			stack.pop(); //TERM
 			stack.pop(); //RELATIONAL
 			stack.pop(); //SIMPLE EXP
+			if(stack.peek() != "ActualParams")
 			stack.pop(); //FOLLOW CHAR -> ; ) ,
 			break;
 		case 32:
 			//END THE EXPRESSION AT HIGHEST RECURSION
 			stack.pop(); //SIMPLE EXP
+			if(stack.peek() != "ActualParams")
 			stack.pop(); //FOLLOW CHAR -> ; ) ,
 			break;
+			
+		case 33:
+			stack.push("Factor");
+			stack.push("MultiplicativeOp");  
+			break;
+			
+		case 34:
+			
+			if(next_token.type== "Opening_Bracket") {
+				//function call
+				stack.push("Factor");
+				stack.push("MultiplicativeOp");
+				stack.push("Closing_Bracket");
+				stack.push("ActualParams");
+				stack.push("Opening_Bracket");
+			}else {
+				stack.push("Factor");
+				stack.push("MultiplicativeOp");  
+			}
+			
+			break;
+		case 35:
+			//sub-expression
+			stack.push("Factor");
+			stack.push("MultiplicativeOp");
+			stack.push("Closing_Bracket");
+			stack.push("Expression");
+			break;
+			
+		case 36:
+			
+			stack.push("Factor");
+			stack.push("MultiplicativeOp");  
+			stack.push("Expression");
+			break;
+			
+		case 37:
+			stack.push("Term");
+			stack.push("AdditiveOp"); 
+			break;
+			
+		case 38:
+			
+			if(next_token.type== "Opening_Bracket") {
+				//function call
+				stack.push("Term");
+				stack.push("AdditiveOp");
+				stack.push("Closing_Bracket");
+				stack.push("ActualParams");
+				stack.push("Opening_Bracket");
+			}else {
+				stack.push("Term");
+				stack.push("AdditiveOp");
+			}
+			
+			break;
+		case 39:
+			//sub-expression
+			stack.push("Term");
+			stack.push("AdditiveOp");
+			stack.push("Closing_Bracket");
+			stack.push("Expression");
+			break;
+			
+		case 40:
+			stack.push("Term");
+			stack.push("AdditiveOp");
+			stack.push("Expression");
+			break;
+		case 41:
+			stack.push("SimpleExpression");
+			stack.push("RelationalOp"); 
+			break;
+			
+		case 42:
+			
+			if(next_token.type	== "Opening_Bracket") {
+				//function call
+				stack.push("SimpleExpression");
+				stack.push("RelationalOp");
+				stack.push("Term");
+				stack.push("AdditiveOp");
+				stack.push("Factor");
+				stack.push("MultiplicativeOp");
+				stack.push("Closing_Bracket");
+				stack.push("ActualParams");
+				stack.push("Opening_Bracket");
+			}else {
+				stack.push("SimpleExpression");
+				stack.push("RelationalOp");
+				stack.push("Term");
+				stack.push("AdditiveOp");
+				stack.push("Factor");
+				stack.push("MultiplicativeOp");  
+			}
+			
+			break;
+		case 43:
+			//sub-expression
+			stack.push("SimpleExpression");
+			stack.push("RelationalOp");
+			stack.push("Term");
+			stack.push("AdditiveOp");
+			stack.push("Factor");
+			stack.push("MultiplicativeOp");
+			stack.push("Closing_Bracket");
+			stack.push("Expression");
+			break;
+			
+		case 44:
+			stack.push("SimpleExpression");
+			stack.push("RelationalOp");
+			stack.push("Term");
+			stack.push("AdditiveOp");
+			stack.push("Factor");
+			stack.push("MultiplicativeOp");  
+			stack.push("Expression");
+			break;
+		
+		case 45:
+			stack.push("AdditiveOp");
+			stack.push("Factor");
+			break;
+		
+		case 46:
+			stack.push("RelationalOp");
+			stack.push("Term");
+			stack.push("AdditiveOp");
+			stack.push("Factor");
+			break;
+		case 47:
+			stack.push("RelationalOp");
+			stack.push("Term");
+			break;
+			
+		case 48:
+			stack.push("ActualParams");
+			stack.push("SimpleExpression");
+			stack.push("RelationalOp");
+			stack.push("Term");
+			stack.push("AdditiveOp");
+			stack.push("Factor");
+			stack.push("MultiplicativeOp");  
+			break;
+			
+		case 49:
+			
+			if(next_token.type== "Opening_Bracket") {
+				//function call
+				stack.push("ActualParams");
+				stack.push("SimpleExpression");
+				stack.push("RelationalOp");
+				stack.push("Term");
+				stack.push("AdditiveOp");
+				stack.push("Factor");
+				stack.push("MultiplicativeOp");
+				stack.push("Closing_Bracket");
+				stack.push("ActualParams");
+				stack.push("Opening_Bracket");
+			}else {
+				stack.push("ActualParams");
+				stack.push("SimpleExpression");
+				stack.push("RelationalOp");
+				stack.push("Term");
+				stack.push("AdditiveOp");
+				stack.push("Factor");
+				stack.push("MultiplicativeOp");  
+			}
+			
+			break;
+		case 50:
+			//sub-expression
+			stack.push("ActualParams");
+			stack.push("SimpleExpression");
+			stack.push("RelationalOp");
+			stack.push("Term");
+			stack.push("AdditiveOp");
+			stack.push("Factor");
+			stack.push("MultiplicativeOp");
+			stack.push(")");
+			stack.push("Expression");
+			break;
+			
+		case 51:
+			stack.push("ActualParams");
+			stack.push("SimpleExpression");
+			stack.push("RelationalOp");
+			stack.push("Term");
+			stack.push("AdditiveOp");
+			stack.push("Factor");
+			stack.push("MultiplicativeOp");  
+			stack.push("Expression");
+			break;
+		case 52:
+			stack.push("ActualParams");
+			stack.push("Expression");
+			break;
+		case 53:
+			stack.pop(); //no more params, remove the bracket
+			break;
 		}
+		
+		
+	
 		
 			
 		
@@ -284,11 +488,22 @@ public class Parser {
 		
 		String last_terminal, last_non_terminal;
 		
-		Token current_token;
+		Token current_token,next_token;
+		next_token = lexedTokens.get(0);
+		
+		System.out.println(Arrays.asList(non_terminals).indexOf("ActualParams"));
 		
 		for(int index = 0; index < no_of_tokens; index++) {
 			
 			current_token = lexedTokens.get(index);
+			
+			if(current_token.type == "Single_Line_Comment" || current_token.type == "Multi_Line_Comment") {
+				continue;
+			}
+			
+			if(index != no_of_tokens-1) {
+				next_token = lexedTokens.get(index+1);
+			}
 			
 			System.out.println(stack.peek()+current_token.type);
 			System.out.println("STACK: "+stack);
@@ -340,7 +555,7 @@ public class Parser {
 				}
 				else {
 					
-						parseSyntax(production_rule);
+						parseSyntax(production_rule, next_token);
 		
 					
 					
